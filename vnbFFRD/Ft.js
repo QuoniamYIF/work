cbd3294a9f00e8d3278ea2f7baaa384a1fb8d973
QuoniamYIF <quoniamy@gmail.com>
2016-10-25 18:13:38 +0800

    2016/10/25

--- vnbFFRD/Ft.js
+++ vnbFFRD/Ft.js
@@ -1,5 +1,5 @@
 var TAB = {
-    props: ["tname", "tfoot", "tabletype", "url", "datatype", "width", "height", "colmodel", "rownum", "subdatatype", "surl", "submodel", "groupingview"],
+    props: ["tname", "tfoot", "tabletype", "url", "datatype", "width", "height", "colmodel", "rownum", "subdatatype", "surl", "submodel", "groupingview", "shrinktofit"],
     template: '<div style="margin-left:20px;">' +
         '<table v-bind:id=TNAME></table>' +
         '<div v-bind:id=TFOOT></div>' +
@@ -14,17 +14,39 @@ var TAB = {
         basicT: function() {
             var self = this;
             $(document).ready(function() {
+                // console.log(self.shrinktofit);
                 $("#" + self.TNAME).jqGrid({
                     url: self.url,
                     datatype: self.datatype,
                     colModel: eval(self.colmodel),
-                    viewrecords: true, // show the current page, data rang and total records on the toolbar
+                    viewrecords: false, // show the current page, data rang and total records on the toolbar
                     width: self.width,
                     height: self.height,
                     rowNum: self.rownum,
+                    // shrinkToFit: "true",
                     loadonce: true, // this is just for the demo
+                    sortable: true,
                     pager: "#" + self.TFOOT,
                     caption: "基本表格"
+                });
+
+                $("#sendButton").click(function() {
+                    var gridData = $("#" + self.TNAME).getRowData();
+                    // var postData = JSON.stringify(gridData);
+                    console.log(gridData);
+                });
+
+                $("#" + self.TNAME).navGrid("#" + self.TFOOT, {
+                    edit: true,
+                    add: false,
+                    del: false,
+                    search: false,
+                    refresh: true,
+                    view: false,
+                    align: "left"
+                }, {
+                    closeAfterEdit: true,
+                    focusField: 1
                 });
             });
         },
@@ -41,7 +63,9 @@ var TAB = {
                     rowNum: 10,
                     sortname: 'CustomerID',
                     jsonReader: {
-                        subgrid: { repeatitems: false }
+                        subgrid: {
+                            repeatitems: false
+                        }
                     },
                     subGrid: true, // set the subGrid property to true to show expand buttons for each row
                     subgridtype: 'json', // set the subgrid type to json
@@ -73,9 +97,15 @@ var TAB = {
                     pager: "#" + self.TFOOT,
                     viewrecords: true,
                     grouping: true,
-                    groupingView: { groupField: ['CustomerID'], groupColumnShow: [true], groupText: ['<b>{0}</b>'], groupOrder: ['asc'], groupSummary: [false], groupCollapse: false }
+                    groupingView: {
+                        groupField: ['CustomerID'],
+                        groupColumnShow: [true],
+                        groupText: ['<b>{0}</b>'],
+                        groupOrder: ['asc'],
+                        groupSummary: [false],
+                        groupCollapse: false
+                    }
                 });
-                $("#" + self.TFOOT).jqGrid("navGrid", "#" + self.TFOOT, { add: false, edit: false, del: false });
             });
         }
     },
